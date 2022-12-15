da852216f392cc150767ac2f3a8fca5f0fee9311
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-12-15 16:38:13 +0100

    Aggiunge GET valore input e inizio fun generatore

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+    $letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    $capital_letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    $symbols = ["#","@","*","£","$","%","&","€","^","?","+"];
+    $numbers = [1,2,3,4,5,6,7,8,9,0];
+
+    $psw_length = $_GET['psw-length'];
+
+    function generatePassword($arr1, $arr2, $arr3, $arr4, $leng){
+
+    };
 ?>
 
 <!DOCTYPE html>
@@ -18,20 +28,24 @@
     </head>
     <body>
         
+        <!-- App Titles -->
         <div class="ms-container text-center">
             <h1 class="app-title">Strong Password Generator</h1>
             <h2 class="app-desc">Genera una password sicura</h2>
         </div>
+        <!-- /App Titles -->
 
+        <!-- Results -->
         <div class="ms-container-sm">
             <div class="ms-psw-container">
                 <p class="psw-text">Nessun parametro valido inserito</p>
             </div>
         </div>
+        <!-- /Results -->
 
+        <!-- Form -->
         <div class="ms-container-sm">
-            <!-- Form -->
-            <form class="row" action="">
+            <form class="row" action="" method="GET">
                 <div class="col d-flex flex-column justify-content-center">
                     <h4>Lunghezza Password:</h4>
                     <div class="ms-btns-container">
@@ -40,11 +54,12 @@
                     </div>
                 </div>
                 <div class="col d-flex flex-column align-items-center">
-                    <input class="ms-input-psw" type="number">
+                    <input class="ms-input-psw" type="number" min="4"
+                    name="psw-length">
                 </div>
             </form>
-            <!-- /Form -->
         </div>
+        <!-- /Form -->
 
         <!-- Bootstrap CDN -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
