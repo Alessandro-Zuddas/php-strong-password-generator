0413e548b89827aae4a2d51c1b50f4e90878d07c
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-12-15 16:10:32 +0100

    Aggiunge grafica

--- index.php
+++ index.php
@@ -8,18 +8,46 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap CDN -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+        <!-- Bootstrap CDN -->
         <!-- Mine style -->
         <link rel="stylesheet" href="style.css">
         <!-- /Mine style -->
-        <!-- Bootstrap CDN -->
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-        <!-- /Bootstrap CDN -->
         <title>Password Generator</title>
     </head>
     <body>
         
-        
+        <div class="ms-container text-center">
+            <h1 class="app-title">Strong Password Generator</h1>
+            <h2 class="app-desc">Genera una password sicura</h2>
+        </div>
 
+        <div class="ms-container-sm">
+            <div class="ms-psw-container">
+                <p class="psw-text">Nessun parametro valido inserito</p>
+            </div>
+        </div>
+
+        <div class="ms-container-sm">
+            <!-- Form -->
+            <form class="row" action="">
+                <div class="col d-flex flex-column justify-content-center">
+                    <h4>Lunghezza Password:</h4>
+                    <div class="ms-btns-container">
+                        <button class="btn btn-primary" type="submit">Invia</button>
+                        <button class="btn btn-secondary" type="submit">Annulla</button>
+                    </div>
+                </div>
+                <div class="col d-flex flex-column align-items-center">
+                    <input class="ms-input-psw" type="number">
+                </div>
+            </form>
+            <!-- /Form -->
+        </div>
+
+        <!-- Bootstrap CDN -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
+        <!-- /Bootstrap CDN -->
     </body>
 </html>
